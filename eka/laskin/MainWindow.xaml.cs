ee52bc20d3f64c6c65303d72dd3fa730884191ef
Ahiisi <104354924+Ahiisi@users.noreply.github.com>
2024-02-06 11:17:20 +0200

    ensimmäinen nappi

--- eka/laskin/MainWindow.xaml.cs
+++ eka/laskin/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace laskin
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            testBox.Text = "tervehdys";
+        }
     }
 }
